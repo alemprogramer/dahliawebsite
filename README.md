9b04d8146e4f23df66635c7f9e06411e8741292b
Mosaddik Ali Maruf <malinjr07@gmail.com>
2021-09-06 08:27:59 +0600

    init merge

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 2021-09-06 01:58:11 PM
 React project has been transferred to typescropt template. i'm merging all files here.
 
+2021-09-06 02:23:41 PM
+
+File merged of public and src root
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import FarmPool from './pages/FarmPool/FarmPool';
+import Farm from './pages/FarmPool/Farm';
+import Earn from './pages/Earn/Earn';
+import Deposit from './pages/Earn/Deposit';
+import Withdrawn from './pages/Earn/Withdrawn';
+import Position from './pages/Position/Position';
+import Remove from './pages/Position/Remove';
+import NavbarMain from './pages/NavBar/NavBarMain';
+import NavBarHome from './pages/NavBar/NavBarHome';
+import NavBarSecondary from './pages/NavBar/NavBarSecondary';
+import DropDown from './pages/Dropdown';
+import Footer from './pages/Footer/Footer';
+import Add from './pages/Position/Add';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <DropDown />
+      <Switch>
+        <Route exact path='/'>
+          <NavBarHome />
+          <Home />
+        </Route>
+        <Route exact path='/farmpool'>
+          <NavbarMain page={'Farm Pools'} />
+          <FarmPool />
+        </Route>
+        <Route exact path='/farmpool/farm'>
+          <NavBarSecondary />
+          <Farm />
+        </Route>
+        <Route exact path='/earn'>
+          <NavbarMain page={'Earn'} />
+          <Earn />
+        </Route>
+        <Route exact path='/earn/deposit'>
+          <NavBarSecondary />
+          <Deposit />
+        </Route>
+        <Route exact path='/earn/withdraw'>
+          <NavBarSecondary />
+          <Withdrawn />
+        </Route>
+        <Route exact path='/position'>
+          <NavbarMain page={'Positions'} />
+          <Position />
+        </Route>
+        <Route exact path='/position/remove'>
+          <NavBarSecondary />
+          <Remove />
+        </Route>
+        <Route exact path='/position/add'>
+          <NavBarSecondary />
+          <Add />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
